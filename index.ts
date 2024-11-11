77772eb95a10b99b585096f5d857b6b310d27c0d
Tuğkan Pilka <tugkanpilka@gmail.com>
2024-11-11 23:49:57 +0300

    Remove type definitions from dist and include in main source
    Deleted `dist/index.d.ts` to avoid redundant type definitions. Incorporated necessary type definitions directly in `index.ts` and updated `tsconfig.json` to generate declaration files. Bumped package version to 1.0.3.

--- index.ts
+++ index.ts
@@ -4,12 +4,30 @@ import {
   addDays,
   getMonth,
 } from "date-fns";
-import {
-  IDateDecorator,
-  IDateGenerationStrategy,
-  IDateGroupingStrategy,
-  MonthInfo,
-} from "./dist";
+
+export interface IDateGenerationStrategy<T> {
+  generate(startDate: Date, endDate: Date): T[];
+}
+
+export interface IDateGroupingStrategy<T, S> {
+  group(dates: T[]): S[];
+}
+
+export interface IDateDecorator<T> {
+  decorate(dates: T[]): T[];
+}
+
+export interface DateInfo {
+  date: Date;
+  weekNumber: number;
+  isDummy?: boolean;
+}
+
+export interface MonthInfo<T> {
+  title: string;
+  number: number;
+  data: T[];
+}
 
 export class StandardDateGenerationStrategy<T extends { date: Date }>
   implements IDateGenerationStrategy<T>
